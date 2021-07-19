114e6c770734d1135e624d48afe22a7b87e63798
millertim83 <80060826+millertim83@users.noreply.github.com>
2021-07-19 16:51:17 -0400

    simplified functions, adjust font sizes.

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
+function renderClock() {
+    const timeDisplay = document.getElementById('time-display');
+    const time = new Date;
+    const minute = leadingZero(time.getMinutes());
+    const seconds = leadingZero(time.getSeconds());    
+    const hour = leadingZero(time.getHours());
+    const isAm = hour < 12 || hour === 0;
+    let amPm = isAm ? 'AM' : 'PM';
+    
+    timeDisplay.textContent = `${formatHour(hour)}:${minute}:${seconds} ${amPm}`;
+    
+}
 
-const timeDisplay = document.getElementById('time-display');
-const dateDisplay = document.getElementById('date-display');
-const today = new Date;
+function formatHour(hour) {
+    hour = hour >= 13 ? hour - 12 : hour;
+    hour = hour === 0 ? hour + 12 : hour;
+    return hour;
+}
+
+function leadingZero(number) {
+    return number < 10 ? '0' + number : number;
+}
 
 
 
-
-function tickingClock() {
-    const minute = leadingZero(today.getMinutes());
-    const seconds = leadingZero(today.getSeconds());    
-    const militaryTime = today.getHours();
-    const standardTime =  get12HourTime(militaryTime);
-    const hour = leadingZero(standardTime);
-
+function renderDate() {
+    const dateDisplay = document.getElementById('date-display');
+    const today = new Date;
     const weekday = daysIndex[today.getDay()];
     const month = monthsIndex[today.getMonth()];
     const date = dateWithSuffix(today.getDate());
     const year = today.getFullYear();
-    
-    timeDisplay.textContent = (hour + ":" + minute + ":" + seconds + " " + (militaryTime >= 12 ? 'PM' : 'AM'));
-    dateDisplay.textContent = (weekday + ", " + month + " " + date + ", " + year);
+
+    dateDisplay.textContent = `${weekday}, ${month} ${date }, ${year}`;
 
 }
 
-function leadingZero(time) {
-    if (time < 10) {
-        return ("0" + time);
-    } else return time;
-}
 
-function get12HourTime(militaryTime) {
-    if (militaryTime === 0) {
-        return 12;
-    } else if (militaryTime > 12) {
-        return (militaryTime - 12)
-    } else return militaryTime;
-}
-
-
-
-    
 const daysIndex = ["Sunday", "Monday", "Tuesday", "Wednesday",
                             "Thursday,", "Friday", "Saturday"];
         
@@ -49,26 +45,22 @@ const monthsIndex = ["January", "February", "March", "April", "May", "June", "Ju
         
             
 function dateWithSuffix(date) {
-        if (date == 1 || date == 21 || date == 31) {
-            return dayOfMonth + "st";
-        } else if (date == 2 || date == 22) {
-            return dayOfMonth + "nd";
-        } else if (date == 3 || date == 23) {
-            return date + "rd";
-        } else return date + "th";
+    if (date < 10 || date > 20) {
+        switch (date % 10) {
+            case 1:
+                return `${date}st`;
+            case 2:
+                return `${date}nd`;
+            case 3:
+                return `${date}rd`;
+        }
+    }
+   return `${date}th`;    
 }
  
-tickingClock();
-setInterval(tickingClock(), 1000);
-
-
-
-
-
-    
-
-
-
-
-
-
+renderClock();
+renderDate();
+setInterval(() => {
+    renderClock();
+    renderDate(); 
+}, 1000);
